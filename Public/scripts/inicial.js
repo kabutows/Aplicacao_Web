bb8e558be036c691bef75fdc89481ed18c3dd483
Pedro Barros <pedrokatsbarros@gmail.com>
2020-12-03 18:18:55 -0300

    Adicinada referência na página inicial.html para o script show-items-inicial.js

--- Public/scripts/inicial.js
+++ Public/scripts/inicial.js
@@ -5,18 +5,3 @@ function redirectsToLogin(){
 function redirectsToCadastro(){
     window.location.href = "../html/cadastro.html";
 }
-
-function showExampleItem(){
-   
-    var imageItem;
-    var nameItem;
-    var priceItem;
-
-    imageItem = document.getElementsByClassName("image-item-box");
-    nameItem = document.getElementsByClassName("name-item-box");
-    priceItem = document.getElementsByClassName("price-item-box");
-
-    imageItem[0].src = "../images/imagem_item_exemplo.png";
-    nameItem[0].value = "Cafeteira Oster vermelha 10 litros";
-    priceItem[0].value = "R$ 221,90"
-}
