e99e749c5289a10aede264bc0f0ecc106d4ac8d8
Pedro Barros <pedrokatsbarros@gmail.com>
2020-12-02 22:37:20 -0300

    Desenvolvido script js da página cadastro e colocado evento de submissão dos dados do formulário

--- Public/scripts/cadastro.js
+++ Public/scripts/cadastro.js
@@ -1,13 +1,19 @@
-//TODO: verificar se nome e senha existem no BD (back-end)
+//TODO: verificar se nome de usuário já não existe no BD (back-end)
 function areValidData(){
     var userName = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
+
     if (!isValidUserName(userName)){
         alert("Ops! O nome é inválido. Ele deve ter pelo menos 3 caracteres.");
         return false;
     }
     if (!isValidPassword(password)){
         alert("Ops! A senha é inválida. Ela deve ter pelo menos 7 caracteres.");
+        return false;
+    }
+    if (!isValidConfirPassword(password, confirmPassword)){
+        alert("Ops! As senhas não são iguais.");
         return false;
     }
     return true;
@@ -17,4 +23,7 @@ function isValidUserName(name){
 }
 function isValidPassword(password){
     return password.length >= 7;
+}
+function isValidConfirPassword(password, confirmPassword){
+    return password == confirmPassword;
 }
