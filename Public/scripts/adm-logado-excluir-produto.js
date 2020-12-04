36326ea4bb09d59fd41aa797dfee27b57a8222c4
Pedro Barros <pedrokatsbarros@gmail.com>
2020-12-04 16:20:58 -0300

    Desenvolvida UI e scripts da página adm-logado-excluir-produto

---
+++ Public/scripts/adm-logado-excluir-produto.js
@@ -0,0 +1,21 @@
+function areValidDeleteProductData(){
+    var productName = document.getElementById("productname").value;
+    var price = document.getElementById("price").value;
+
+    if (!isValidProductName(productName)){
+        alert("Ops! O nome é inválido. Ele deve ter pelo menos 3 caracteres.");
+        return false;
+    }
+    if (!isValidPrice(price)){
+        alert("Ops! Você não atribui um preço ao produto.");
+        return false;
+    }
+  
+    return true;
+}
+function isValidProductName(name){
+    return name.length >= 3;
+}
+function isValidPrice(price){
+    return price.length > 0;
+}
