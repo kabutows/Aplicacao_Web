1740df2dde73dcf4492c590d9cf9250118bb83e3
Pedro Barros <pedrokatsbarros@gmail.com>
2020-12-04 16:30:26 -0300

    Desenvolvido script dos botões de inclusão, alterar e exclusão das páginas de adm de usuário e produto

---
+++ Public/scripts/inicial-adm-logado-botoes.js
@@ -0,0 +1,11 @@
+function redirectsToUserInclusionPage(){
+    window.location.href = "../html/adm-logado-incluir-usuario.html";
+}
+
+function redirectsToUserEditPage(){
+    window.location.href = "../html/adm-logado-alterar-usuario.html";
+}
+
+function redirectsToUserDeletePage(){
+    window.location.href = "../html/adm-logado-excluir-usuario.html";
+}
